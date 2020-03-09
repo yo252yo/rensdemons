08ef3d90775da7f8a14284dd5cf975fb43d3ed69
yoann <yo252yo@gmail.com>
2020-03-09 13:47:20 +0100

    better color names

--- levels/zero.js
+++ levels/zero.js
@@ -1,23 +1,23 @@
 
-var color2 = Color.random().hoffset(0.4);
-var color1 = color2.opposite();
-var color3 = color2.hoffset(-0.25);
-var color4 = color3.hoffset(-0.15);
+var color_background = Color.random().hoffset(0.45);
+var color_player = color_background.opposite();
+var color_obj_light = color_background.hoffset(-0.25);
+var color_obj_dark = color_obj_light.hoffset(-0.2);
 
-document.body.style.backgroundColor = color1.code();
+document.body.style.backgroundColor = color_player.code();
 
-new Rectangle(10,450,500,400, color2.code());
+new Rectangle(10,450,500,400, color_background.code());
 
-(new StaticSprite("testing/char1.png", color1.code())).move(100,100);
+(new StaticSprite("testing/char1.png", color_player.code())).move(100,100);
 
-(new StaticSprite("testing/tree.png", color3.code())).move(20,0);
-(new StaticSprite("testing/tree.png", color3.code())).move(150,30);
-(new StaticSprite("testing/tree.png", color3.code())).move(290,60);
+(new StaticSprite("testing/tree.png", color_obj_light.code())).move(20,0);
+(new StaticSprite("testing/tree.png", color_obj_light.code())).move(150,30);
+(new StaticSprite("testing/tree.png", color_obj_light.code())).move(290,60);
 
 
-(new StaticSprite("testing/house.png", color4.code())).move(150,30);
+(new StaticSprite("testing/house.png", color_obj_dark.code())).move(150,30);
 
-(new StaticSprite("testing/tree.png", color3.code())).move(240,280);
+(new StaticSprite("testing/tree.png", color_obj_light.code())).move(240,280);
 
 /*
 var style = document.createElement('style');
