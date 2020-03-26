943ffa461ae62f0294ee70040f011964b9c37580
yoann <yo252yo@gmail.com>
2020-03-26 10:13:10 +0100

    Make name generation modular

--- js/modules/DICTIONARY.js
+++ js/modules/DICTIONARY.js
@@ -10,9 +10,21 @@ class DisctionaryInternal {
   }
 
   initialize() {
-    this.sidekick_name = MARKOV_MODELS.human_names.mutate("Aerith", 10);
-    this.world_name = MARKOV_MODELS.human_names.mutate("Hyrule", 20);
+    for(var propt in this){
+      if(propt.startsWith("_gen")){
+        this[propt]();
+      }
+    }
   }
+
+  // Generators
+  _gen_sidekick_name = function() {
+    this.sidekick_name = MARKOV_MODELS.human_names.mutate("Aerith", 10);
+  };
+
+  _gen_world_name = function() {
+    this.world_name = MARKOV_MODELS.human_names.mutate("Hyrule", 20);
+  };
 }
 
 const DICTIONARY = {
