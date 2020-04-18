84ca743d0f0edaf8bd09291588abb886405a78b6
yoann <yo252yo@gmail.com>
2020-04-18 14:46:41 +0200

    fix color reset on title screen

--- levels/titlescreen.js
+++ levels/titlescreen.js
@@ -1,19 +1,20 @@
 
 
-var s0 = new StaticSprite("assets/title_layer0.png", 'player');
-var s1 = new StaticSprite("assets/title_layer1.png", 'background');
-var s2 = new StaticSprite("assets/title_layer2.png", 'obj_light');
-/*
-LEVEL.index_object({visual_element: s0});
-LEVEL.index_object({visual_element: s1});
-LEVEL.index_object({visual_element: s2});*/
+var s0 = new LevelObject(new StaticSprite("assets/title_layer0.png", 'player'), 0, SCREEN.height());
+var s1 = new LevelObject(new StaticSprite("assets/title_layer1.png", 'background'), 0, SCREEN.height());
+var s2 = new LevelObject(new StaticSprite("assets/title_layer2.png", 'obj_light'), 0, SCREEN.height());
+
 
 function adapt_sprite(s, depth) {
-  s.container.style.top = "0px";
-  s.container.style.left =  "0px";
-  s.container.style.height = "100%";
-  s.html_canvas.style.height = "100%";
-  s.adjust_depth(depth);
+  s.visual_element.container.style.top = "0px";
+  s.visual_element.container.style.left =  "0px";
+  s.visual_element.container.style.height = "100%";
+  s.visual_element.html_canvas.style.height = "100%";
+  s.visual_element.adjust_depth(depth);
+
+  s.visual_element.draw = function (){
+    s.visual_element.tint();
+  }
 }
 
 // We wait so that we're sure the sprite has been drawn :(
