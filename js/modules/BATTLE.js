0520d1c907fdd12b82c05860d5c215d04276ead1
yoann <yo252yo@gmail.com>
2020-05-11 12:58:52 +0200

    adds ending animation and basic refactoring of battle

--- js/modules/BATTLE.js
+++ js/modules/BATTLE.js
@@ -6,14 +6,6 @@ const BATTLE = {
   previous_position: undefined,
   callback: undefined,
 
-  clear: function() {
-    BATTLE.player_actions = [];
-    BATTLE.monster_actions = [];
-    BATTLE.callback = undefined;
-    BATTLE.previous_position = undefined;
-    BATTLE.current_battle = "";
-  },
-
   player_turn: function() {
     var options = [];
     for (var i in BATTLE.player_actions){
@@ -66,68 +58,105 @@ const BATTLE = {
     BATTLE.monster_turn(text);
   },
 
-  end: function(text) {
-    PALETTE.color_interface();
-  },
-
-  _loss: function() {
-    BATTLE.end();
-    setTimeout ( function() { LEVEL.setup("gameover");}, 1000);
-  },
-
   loss: function() {
-    setTimeout (BATTLE._loss, 1000);
-  },
-
-  _win: function(text) {
-    BATTLE.end();
-    LEVEL.factory.import(BATTLE.previous_position);
-
-    if (BATTLE.callback){
-      setTimeout(BATTLE.callback, 200);
-    }
+    BATTLE.manager.teardown.start(BATTLE.manager.teardown.loss);
   },
 
   win: function(text) {
-    setTimeout (BATTLE._win, 1000);
-  },
-
-  setup_animation: function (previous_position) {
-      var pos = previous_position.saved_character;
-      var html_rectangle = document.createElement('div');
-      html_rectangle.style.background =  PALETTE.color('obj_dark').code();
-      html_rectangle.style.top = pos[1] + "px";
-      html_rectangle.style.left = pos[0] + "px";
-      html_rectangle.class = "expanding_div";
-      html_rectangle.classList.add("expanding_div");
-      LEVEL.html().appendChild(html_rectangle);
-      console.log(html_rectangle);
-  },
-
-  _setup: function(name, callback, previous_position) {
-    LEVEL.clear();
-    BATTLE.clear();
-    PALETTE.color_for_battle();
-    BATTLE.previous_position = previous_position;
-    BATTLE.current_battle = name;
-
-    if(callback) {
-      BATTLE.callback = callback;
-    }
-
-    new Import("battles/" + name);
-    CONSOLE.sys_log("- Loaded battle " + name);
+    BATTLE.manager.teardown.start(BATTLE.manager.teardown.win);
   },
 
   setup: function(name, callback, previous_position) {
-    IO.control.cede();
+    BATTLE.manager.setup.start(name, callback, previous_position);
+  },
 
-    if (!previous_position) {
-      previous_position = LEVEL.factory.export();
-    }
+  manager: {
+    clear: function() {
+      console.log("A");
+      BATTLE.player_actions = [];
+      BATTLE.monster_actions = [];
+      BATTLE.callback = undefined;
+      BATTLE.previous_position = undefined;
+      BATTLE.current_battle = "";
+    },
 
-    BATTLE.setup_animation(previous_position);
-    setTimeout ( function() { BATTLE._setup(name, callback, previous_position); }, 1000);
+    setup: {
+      start: function(name, callback, previous_position) {
+        BATTLE.manager.clear();
+        IO.control.cede();
+
+        if (!previous_position) {
+          previous_position = LEVEL.factory.export();
+        }
+
+        BATTLE.previous_position = previous_position;
+        BATTLE.manager.setup.animation();
+        setTimeout ( function() { BATTLE.manager.setup.end(name, callback); }, 1000);
+      },
+
+      animation: function () {
+          var pos = BATTLE.previous_position.saved_character;
+          var html_rectangle = document.createElement('div');
+          html_rectangle.style.background =  PALETTE.color('obj_dark').code();
+          html_rectangle.style.top = pos[1] + "px";
+          html_rectangle.style.left = pos[0] + "px";
+          html_rectangle.classList.add("expanding_div");
+          LEVEL.html().appendChild(html_rectangle);
+          console.log(html_rectangle);
+          // destroy
+      },
+
+      end: function(name, callback) {
+        LEVEL.clear();
+        PALETTE.color_for_battle();
+        BATTLE.current_battle = name;
+
+        if(callback) {
+          BATTLE.callback = callback;
+        }
+
+        new Import("battles/" + name);
+        CONSOLE.sys_log("- Loaded battle " + name);
+      },
+    },
+    
+    teardown: {
+      start: function(ending) {
+        PALETTE.color_interface();
+        LEVEL.clear();
+        BATTLE.manager.teardown.animation();
+        setTimeout (function() {BATTLE.manager.teardown.end(ending);}, 1000);
+      },
+
+      animation: function () {
+        console.log("AAA");
+          var pos = BATTLE.previous_position.saved_character;
+          var html_rectangle = document.createElement('div');
+          html_rectangle.style.background =  PALETTE.color('obj_dark').code();
+          html_rectangle.style.top = pos[1] + "px";
+          html_rectangle.style.left = pos[0] + "px";
+          html_rectangle.classList.add("collapsing_div");
+          LEVEL.html().appendChild(html_rectangle);
+          console.log(html_rectangle);
+          // destroy
+      },
+
+      end: function(ending) {
+        ending();
+      },
+
+      loss: function() {
+        LEVEL.setup("gameover");
+      },
+
+      win: function() {
+        LEVEL.factory.import(BATTLE.previous_position);
+
+        if (BATTLE.callback){
+          setTimeout(BATTLE.callback, 200);
+        }
+      },
+    },
   },
 
   reload: function(){
