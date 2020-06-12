86ce3b7ee0c8192effd00e575cb43c4a6eb8d6f1
yoann <yo252yo@gmail.com>
2020-06-12 15:13:35 +0200

    adds volumne control

--- js/modules/AUDIO.js
+++ js/modules/AUDIO.js
@@ -1,8 +1,11 @@
 
 const AUDIO = {
   _TRACKS: {},
+  VOLUME: {
+    SFX: 0.8,
+  },
 
-  _sound: function (track){
+  _load_sound: function (track){
     AUDIO._TRACKS[track] = new Audio('assets/sounds/' + track + '.wav');
   },
 
@@ -18,17 +21,20 @@ const AUDIO = {
   _play_buffered: function(track){
     AUDIO._TRACKS[track].play();
   },
-  _play: function(track){
-    (new Audio('assets/sounds/' + track + '.wav')).play();
+
+  _play_sfx: function(track){
+    var audio = new Audio('assets/sounds/' + track + '.wav');
+    audio.volume = AUDIO.VOLUME.SFX;
+    audio.play();
   },
 
   effect: {
-    levelup: function() { AUDIO._play('lvlup'); },
-    unlock: function() {  AUDIO._play('unlock'); },
-    clickmove: function() { AUDIO._play('clickmove'); },
-    page: function() { AUDIO._play('page'); },
-    choice: function() { AUDIO._play('choice'); },
-    interaction: function() { AUDIO._play('interaction'); },
+    levelup: function() {     AUDIO._play_sfx('lvlup'); },
+    unlock: function() {      AUDIO._play_sfx('unlock'); },
+    clickmove: function() {   AUDIO._play_sfx('clickmove'); },
+    page: function() {        AUDIO._play_sfx('page'); },
+    choice: function() {      AUDIO._play_sfx('choice'); },
+    interaction: function() { AUDIO._play_sfx('interaction'); },
   },
 };
 
