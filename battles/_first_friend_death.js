46e14c9116c11ff2154c6a594498729d2ed7d78a
yoann <yo252yo@gmail.com>
2020-07-13 16:54:43 +0200

    Make the child corpse into its own battle

---
+++ battles/_first_friend_death.js
@@ -0,0 +1,11 @@
+// ===================
+// =================== INITIALIZATION
+// ===================
+new CenteredImage("assets/battles/child_m.png", 'background');
+
+PLAYER_ACTIONS.default_useless.flee();
+
+// ===================
+// =================== START
+// ===================
+BATTLE.operations.start("You find on the ground the corpse of $$child_friends_m1$. It's obviously in a pretty bad shape, the Basilisk really did a number on him. You avert your eyes from the bloody remains. It's too much to handle.");
--- levels/003_trial.js
+++ levels/003_trial.js
@@ -5,7 +5,7 @@ new S_Statue(1655, 195);
 new S_Statue(1585, 1850);
 new S_Statue(830, 1025);
 
-new SE_battle(1500, 950, "&You find on the ground the corpse of $$child_friends_m1$. It's obviously in a pretty bad shape, the Basilisk really did a number on him. You avert your eyes from the bloody remains. It's too much to handle.");
+new SE_battle(1500, 950, "_first_friend_death");
 
 
 CURRENTLEVEL.initialize_with_character(1650, 975);
