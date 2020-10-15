1088721641a73227b5456401db7869799130899e
yoann <yo252yo@gmail.com>
2020-10-16 01:49:45 +0200

    implements the remaining martyrdom effects on dodge capacity

--- js/modules/BATTLE/DODGE.js
+++ js/modules/BATTLE/DODGE.js
@@ -9,11 +9,29 @@ const DODGE = {
   },
   defense_angle: undefined,
   attack_angle: undefined,
-  params: {
+  _params: {
     attack_amplitude: 0.1, // Between 0 and 1
     warning_time_s: 1,
     react_time_s: 0.5,
     time_variation: 0.1, // 1 = 100%
+  },
+
+  get_params: {
+    attack_amplitude: function() {
+      return DODGE._params.attack_amplitude / (1+10*MARTYRDOM.effect(MARTYRDOMS.Elusiveness));
+    },
+
+    warning_time_s: function() {
+      return DODGE._params.warning_time_s * (1+3*MARTYRDOM.effect(MARTYRDOMS.Foresight))  + 3 * MARTYRDOM.effect(MARTYRDOMS.Foresight);
+    },
+
+    react_time_s: function() {
+      return DODGE._params.react_time_s * (1+1*MARTYRDOM.effect(MARTYRDOMS.Reflex)) + 1 * MARTYRDOM.effect(MARTYRDOMS.Reflex);
+    },
+
+    time_variation: function() {
+      return DODGE._params.time_variation;
+    },
   },
 
   init: function() {
@@ -50,7 +68,7 @@ const DODGE = {
       var attack_angle = DODGE.attack_angle * Math.PI * 2;
       var x = mid + r * Math.cos(attack_angle);
       var y = mid - r * Math.sin(attack_angle);
-      var gradius = multiplier * DODGE.sprite.prompt.width*(0.2 + DODGE.params.attack_amplitude * 1.7);
+      var gradius = multiplier * DODGE.sprite.prompt.width*(0.2 + DODGE.get_params.attack_amplitude() * 1.7);
       HTML.canvas.draw_gradient_in(DODGE.sprite.attacked.html_canvas, color, x, y, gradius);
     },
 
@@ -98,11 +116,11 @@ const DODGE = {
 
     compute: function (){
       if(DODGE.defense_angle == undefined) { return false; }
-      if(!DODGE.attack_angle || !DODGE.params.attack_amplitude) { return true; }
+      if(!DODGE.attack_angle || !DODGE.get_params.attack_amplitude()) { return true; }
 
       var diff = Math.abs(DODGE.attack_angle - DODGE.defense_angle);
       var diff2 = Math.abs(1 + DODGE.attack_angle - DODGE.defense_angle);
-      return (Math.min(diff, diff2) > (DODGE.params.attack_amplitude / 2));
+      return (Math.min(diff, diff2) > (DODGE.get_params.attack_amplitude() / 2));
     },
 
     success: function(){
@@ -147,8 +165,8 @@ const DODGE = {
   events:{
     prompt: function(){
       DODGE.draw.prompt();
-      var warning_time = DODGE.params.warning_time_s * 1000;
-      var rand_teak = 2 * (Math.random() - 0.5) * DODGE.params.time_variation * warning_time;
+      var warning_time = DODGE.get_params.warning_time_s() * 1000;
+      var rand_teak = 2 * (Math.random() - 0.5) * DODGE.get_params.time_variation() * warning_time;
       warning_time = Math.max (DODGE.MIN_TIMEOUT, warning_time + rand_teak);
       setTimeout(DODGE.events.react, warning_time);
     },
@@ -159,8 +177,8 @@ const DODGE = {
       DODGE.attack_target = Math.random();
       DODGE.draw.hit();
 
-      var react_time = DODGE.params.react_time_s * 1000;
-      var rand_teak = 2 * (Math.random() - 0.5) * DODGE.params.time_variation * react_time;
+      var react_time = DODGE.get_params.react_time_s() * 1000;
+      var rand_teak = 2 * (Math.random() - 0.5) * DODGE.get_params.time_variation() * react_time;
       react_time = Math.max (DODGE.MIN_TIMEOUT, react_time + rand_teak);
       setTimeout(DODGE.events.hit, react_time);
     },
@@ -169,7 +187,7 @@ const DODGE = {
       IO.control.cede();
       DODGE.draw.hit_confirm();
 
-      var str = "attack at " + DODGE.attack_angle + " with amplitude " + DODGE.params.attack_amplitude + " defending at " + DODGE.defense_angle;
+      var str = "attack at " + DODGE.attack_angle + " with amplitude " + DODGE.get_params.attack_amplitude() + " defending at " + DODGE.defense_angle;
       if (DODGE.outcome.compute()){
         CONSOLE.log.battle("[DODGE] " + str );
         setTimeout(DODGE.outcome.success, DODGE.TIME_SHOWING_RESULTS_BEFORE_CLEANUP);
@@ -182,7 +200,7 @@ const DODGE = {
 
   absorb_param: function(params, name) {
     if(name[params]){
-      DODGE.params.name = params.name;
+      DODGE._params.name = params.name;
     }
   },
 
