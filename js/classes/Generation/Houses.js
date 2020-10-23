0f53404b4a47a1eb50b8a0b9fc198eaf97ccdb61
yoann <yo252yo@gmail.com>
2020-10-23 18:08:22 +0200

    makes a dedicated class for rooms

--- js/classes/Generation/Houses.js
+++ js/classes/Generation/Houses.js
@@ -1,3 +1,52 @@
+class HG_Room {
+    constructor(gen, x, y) {
+      this.gen = gen;
+      this.MAX_ROOM_W = 500;
+      this.MAX_ROOM_H = 500;
+      this.MIN_ROOM_W = 100;
+      this.MIN_ROOM_H = 100;
+      this.x = x;
+      this.y = y;
+      this.rng_size();
+      this.draw();
+    }
+
+    rng_size(x, y) {
+      this.w = this.MIN_ROOM_W + (this.MAX_ROOM_W - this.MIN_ROOM_W) * this.gen.get();
+      this.h = this.MIN_ROOM_H + (this.MAX_ROOM_H - this.MIN_ROOM_H) * this.gen.get();
+    }
+
+    draw() {
+      new S_Floor(this.x, this.y, this.w, this.h);
+    }
+
+    expand_top() {
+       var room_top = new HG_Room(this.gen, this.x, this.y - this.h - 25);
+       var connection_x = this.x + Math.min(room_top.w, this.w)/2 - 25;
+       new S_Floor(connection_x, this.y - this.h + 25, 50, 75);
+    }
+
+    expand_right() {
+       var room_right = new HG_Room(this.gen, this.x + this.w + 25, this.y);
+       var connection_y = this.y - Math.min(room_right.h, this.h)/2 + 25;
+       new S_Floor(this.x + this.w - 25, connection_y, 75, 50);
+    }
+
+    expand(){
+      if (this.gen.get() > 0.4) { // room above?
+         this.expand_top();
+      }
+
+      if (this.gen.get() > 0.4) { // room right
+         this.expand_right();
+      }
+    }
+
+    main_entrance() {
+      return [this.x + this.w / 2, this.y - 25];
+    }
+}
+
 
 class HouseGenerator {
     constructor(seed) {
@@ -10,22 +59,10 @@ class HouseGenerator {
       this.y = 1000;
     }
 
-    build_room(x, y, index) {
-      var w = this.MIN_ROOM_W + (this.MAX_ROOM_W - this.MIN_ROOM_W) * this.gen.get();
-      var h = this.MIN_ROOM_H + (this.MAX_ROOM_H - this.MIN_ROOM_H) * this.gen.get();
-      new S_Floor(x, y, w, h);
-      return [w, h];
-    }
-
     build() {
-      var main_hall = this.build_room(this.x, this.y, 1);
-      if (this.gen.get() > 0.5) { // room above?
-         var room_top = this.build_room(this.x, this.y - main_hall[1] - 25 , 4);
-         var connection_x = this.x + Math.min(room_top[0], main_hall[0])/2 - 25;
-         new S_Floor(connection_x, this.y - main_hall[1] + 25, 50, 75);
-      }
-
-      return [this.x + main_hall[0] / 2, this.y - 25]; // entrance
+      var main_hall = new HG_Room(this.gen, this.x, this.y);
+      main_hall.expand();
+      return main_hall.main_entrance(); // entrance
     }
 
 }
