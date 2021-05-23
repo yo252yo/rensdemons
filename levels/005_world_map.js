f7214afb14d109814afeb5ede795c2b3c63a0d16
yoann <yo252yo@gmail.com>
2021-05-23 19:11:41 +0200

    world map

--- levels/005_world_map.js
+++ levels/005_world_map.js
@@ -3,38 +3,40 @@ AUDIO.music.map();
 
 INTERFACE.make_compass();
 
-new S_Floor(50, 2050, 1000, 2000, 'obj_dark');
+new S_Floor(50, 2550, 3000, 2500, 'obj_dark');
 
 // First make the towns
 new SM_Town(250, 1750, "004_town1", "town_1");
-new SM_Town(550, 550, "006_town2", "town_2");
+new SM_Town(850, 550, "006_town2", "town_2");
 
 
 // do not keep, just a test
-new SM_Town(750, 750, "_town3", "town_3", function() { return true || false;});
-new SM_Town(950, 950, "_town4", "town_4", function() { return true || false;});
-new SM_Town(1150, 1150, "_town5", "town_5", function() { return true || false;});
+new SM_Town(2300, 1275, "_town3", "town_3", function() { return false;});
+new SM_Town(2725, 300, "_town4", "town_4", function() { return false;}); // optional
+new SM_Town(1450, 2500, "_town5", "town_5", function() { return false;}); // optional
 
 var seed = DICTIONARY.get("world_map_seed");
 var gen = new Generator(seed);
 var mapFiller = new Filler(gen);
-mapFiller.set_zone(50, 2050, 1000, 2000);
+mapFiller.set_zone(50, 2550, 3000, 2500);
 
+
+// could be non uniform
 for(var i = 0; i < 3; i++) {
-  mapFiller.set_tries(1, 8);
+  mapFiller.set_tries(3, 25);
   mapFiller.set_object(100, 100, function(x,y,g){ return new SM_Trees(x, y, g);});
   mapFiller.fill_by_retry();
 
-  mapFiller.set_tries(1, 5);
+  mapFiller.set_tries(3, 15);
   mapFiller.set_object(300, 200, function(x,y,g){ return new SM_Lake(x, y, g);});
   mapFiller.fill_by_retry();
 
-  mapFiller.set_tries(1, 5);
+  mapFiller.set_tries(3, 15);
   mapFiller.set_object(300, 150, function(x,y,g){ return new SM_Mountain(x, y, g);});
   mapFiller.fill_by_retry();
 }
 // Place hills after as they are walkable
-mapFiller.set_tries(3, 10);
+mapFiller.set_tries(10, 40);
 mapFiller.set_object(200, 100, function(x,y,g){ return new SM_Hills(x, y, g);});
 mapFiller.fill_by_retry();
 
