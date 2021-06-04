8faade005a9b420dde227e92e191a7a7a019e996
yoann <yo252yo@gmail.com>
2021-06-05 00:27:30 +0200

    beginning of mushrooms

--- levels/023_mushrooms.js
+++ levels/023_mushrooms.js
@@ -1,1 +1,38 @@
 console.log("todo")
+
+var f = new S_Floor(900,2500,200,75, 'obj_dark');
+
+f.interaction = function(){
+  CURRENTLEVEL.setup("010_world_map");
+}
+
+new S_Floor(950,2450,100,500);
+new S_Floor(625,2000,750,50);
+new S_Floor(625,2250,50,525);
+new S_Floor(1325,2150,50,600);
+new S_Floor(975,1975,50,375);
+new S_Floor(1175,1600,475,50);
+new S_Floor(1150,2150,400,50);
+new S_Floor(1500,2350,50,425);
+new S_Floor(1500,2350,300,50);
+new S_Floor(1750,2500,50,325);
+new S_Floor(1750,2500,300,50);
+new S_Floor(1750,2225,200,50);
+new S_Floor(1500,1975,525,50);
+new S_Floor(1975,2075,50,350);
+new S_Floor(1975,2075,275,50);
+new S_Floor(1975,1775,250,50);
+new S_Floor(1600,1725,50,400);
+new S_Floor(1600,1725,225,50);
+new S_Floor(1600,1375,375,50);
+new S_Floor(1925,1525,50,400);
+new S_Floor(1925,1525,275,50);
+new S_Floor(475,2250,200,50);
+new S_Floor(475,2375,50,275);
+new S_Floor(250,2150,275,50);
+new S_Floor(250,2275,50,300);
+new S_Floor(400,1775,275,50);
+new S_Floor(400,1950,50,425);
+new S_Floor(200,1575,250,50);
+
+CURRENTLEVEL.initialize_with_character(1000, 2425);
