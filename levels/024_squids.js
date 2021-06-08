0c71beda96e64e49419c1c54134a255db17267ee
yoann <yo252yo@gmail.com>
2021-06-08 19:35:06 +0200

    puts monster and avoids drinking potion for every fight

--- levels/024_squids.js
+++ levels/024_squids.js
@@ -1,6 +1,15 @@
+var access = "_squid_access";
 
-new S_Floor(900,2525,200,125, 'obj_dark', '010_world_map');
-new S_Floor(950,2650,100,200, 'obj_dark', '010_world_map');
+
+var leave = function(){
+  CURRENTLEVEL.setup('010_world_map');
+  INVENTORY.decrease(access);
+}
+var f1 = new S_Floor(900,2525,200,125, 'obj_dark');
+var f2 = new S_Floor(950,2650,100,200, 'obj_dark');
+
+f1.interaction = leave;
+f2.interaction = leave;
 
 new S_Floor(375,2400,1275,875);
 new S_Floor(275,2475,200,175);
@@ -17,10 +26,33 @@ new S_Floor(875,2575,100,75);
 
 console.log("todo Ancient Armament Armature");
 
+
+var gen = new Generator(DICTIONARY.get("dungeons_seed"));
+var filler = new Filler(gen);
+filler.set_zone(150,2475,2100,1475);
+
+filler.set_tries(40, 100);
+filler.set_event([
+  function(x,y,g){ return new SBattle(x, y, 'waters/anemone')},
+  function(x,y,g){ return new SBattle(x, y, 'waters/crab')},
+  function(x,y,g){ return new SBattle(x, y, 'waters/anglerjelly')},
+  function(x,y,g){ return new SBattle(x, y, 'waters/jellyfish')},
+  function(x,y,g){ return new SBattle(x, y, 'waters/octopus')},
+  function(x,y,g){ return new SBattle(x, y, 'waters/squid')},
+//  new SB_rubble(x, y, ITEM.Elixir_fire),
+//  new SE_small_treasure(x, y, ITEM.Stone),
+//  new SB_event(x, y, '...'),
+]);
+filler.fill_by_retry();
+
+
+
+
 var dive = function(){
   new CenteredTextMenu("Drink a breathing potion?", [
     {"text": "Yes", "effect": function() {
       INVENTORY.decrease(ITEM.BreathingPotion);
+      INVENTORY.increase(access);
       IO.control.character();
     }},
     {"text": "No", "effect": function() {
@@ -31,6 +63,10 @@ var dive = function(){
 
 CURRENTLEVEL.initialize_with_character(1000, 2425);
 var potion = function(){
+  if (INVENTORY.count(access)){
+    IO.control.character();
+    return;
+  }
   if (INVENTORY.count(ITEM.BreathingPotion) > 0){
     TextBannerSequence.make([
       `$$BestFriend$: "So we're supposed to dive in and find... what?"`,
