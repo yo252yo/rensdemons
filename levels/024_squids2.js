4eb6d2cb8170f051aab44991177b1fe577a5155f
yoann <yo252yo@gmail.com>
2021-06-09 23:57:28 +0200

    whale inside map

--- levels/024_squids2.js
+++ levels/024_squids2.js
@@ -1,5 +1,31 @@
 AUDIO.music.levels.squids();
 
+new S_Floor(75,375,625,50);
+new S_Floor(650,700,350,375);
+new S_Floor(1050,850,275,525);
+new S_Floor(950,375,150,50);
+new S_Floor(1275,375,225,50);
+new S_Floor(1450,450,50,125);
+new S_Floor(1350,450,150,50);
+new S_Floor(1350,525,50,125);
+new S_Floor(1350,525,225,50);
+new S_Floor(1525,525,50,200);
+new S_Floor(1525,375,125,50);
+new S_Floor(1600,675,50,350);
+new S_Floor(1525,675,125,50);
+new S_Floor(1525,675,50,125);
+new S_Floor(1450,600,125,50);
+new S_Floor(1450,750,50,200);
+new S_Floor(1450,750,200,50);
+new S_Floor(1600,825,50,125);
+new S_Floor(1450,825,200,50);
+new S_Floor(1450,900,50,125);
+new S_Floor(1450,900,275,50);
+new S_Floor(1675,900,100,575);
+new S_Floor(1725,425,125,100);
 
-CURRENTLEVEL.initialize_with_character(1000, 2425);
+new S_Floor(1800,425,75,100, 'obj_dark', '024_squids');
+
+
+CURRENTLEVEL.initialize_with_character(100, 350);
 IO.control.character();
