58ccd6ee02fc430f20243866816cc36a42cb98bb
yoann <yo252yo@gmail.com>
2021-06-12 02:04:12 +0200

    begin harpies2

--- levels/025_harpies2.js
+++ levels/025_harpies2.js
@@ -28,4 +28,31 @@ new S_Floor(700,825,75,50);
 new S_Floor(700,1000,75,50);
 new S_Floor(700,1175,75,50);
 
+
+
+var bed = function (x, y) {
+  var b = new B_Bed(x+50,y-95);
+  b.interaction = function() {
+    TextBannerSequence.make([
+      "There's a bed, but... it's made of metal? That doesn't seem very comfortable, but it did stand the test of time.",
+    ]);
+   };
+}
+
+bed(1025,1225,150,150);
+bed(575,1225,150,150);
+
+bed(1025,1050,150,150);
+bed(575,1050,150,150);
+
+bed(1025,875,150,150);
+bed(575,875,150,150);
+
+bed(1025,700,150,150);
+bed(575,700,150,150);
+
+
+console.log("todo Ancient Armament Ammunition");
+
+
 CURRENTLEVEL.initialize_with_character(850, 1225);
