9b9b248d7b2583e02a60357c6cf2bc0190bc2d3e
yoann <yo252yo@gmail.com>
2021-06-18 16:21:16 +0200

    placeholder default attack

--- battles/forests/fungus.js
+++ battles/forests/fungus.js
@@ -15,6 +15,17 @@ BATTLE.operations.add_loot(ITEM.Mushroom, 1);
 // ===================
 //hack MONSTER BEHAVIOR
 // ===================
+var attack = {
+  attack_amplitude: 0.1, // Between 0 and 1
+  warning_time_s: 2,
+  react_time_s: 1,
+  time_variation: 0.5, // 1 = 100%
+};
+
+BATTLE.monster_actions.add_textual("...", attack);
+BATTLE.monster_actions.add_textual("...", attack);
+BATTLE.monster_actions.add_textual("...", attack);
+
 
 
 // ===================
--- battles/mountains/harpy.js
+++ battles/mountains/harpy.js
@@ -1,3 +1,14 @@
+
+var attack = {
+  attack_amplitude: 0.1, // Between 0 and 1
+  warning_time_s: 2,
+  react_time_s: 1,
+  time_variation: 0.5, // 1 = 100%
+};
+
+BATTLE.monster_actions.add_textual("...", attack);
+BATTLE.monster_actions.add_textual("...", attack);
+BATTLE.monster_actions.add_textual("...", attack);
 // ===================
 //hack INITIALIZATION
 // ===================
@@ -15,6 +26,17 @@ BATTLE.operations.add_loot("", 2);
 // ===================
 //hack MONSTER BEHAVIOR
 // ===================
+var attack = {
+  attack_amplitude: 0.1, // Between 0 and 1
+  warning_time_s: 2,
+  react_time_s: 1,
+  time_variation: 0.5, // 1 = 100%
+};
+
+BATTLE.monster_actions.add_textual("...", attack);
+BATTLE.monster_actions.add_textual("...", attack);
+BATTLE.monster_actions.add_textual("...", attack);
+
 
 
 // ===================
