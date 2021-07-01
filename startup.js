0bad5cad6764cd6b6767299aab6b49101fd9ef94
yoann <yo252yo@gmail.com>
2021-07-01 16:22:48 +0200

    add webkit disclaimer

--- startup.js
+++ startup.js
@@ -7,5 +7,10 @@ if(loading_div){
   loading_div.parentNode.removeChild(loading_div);
 }
 
+
 DISK.initialize_game();
 CURRENTLEVEL.setup("titlescreen");
+
+if (! typeof Android === "undefined") {
+    CONSOLE.error("It appears that your web browser is not allowed to download data files. You can continue to the game with the autosave, but you will not be able to download your save files. I recommend you use a dedicated web browser (Firefox, Opera, Chrome...) so that you can download your saves.", false, true);
+}
