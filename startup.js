26470bd00d257d31d9113b91891a30024d654623
yoann <yo252yo@gmail.com>
2021-07-01 16:49:48 +0200

    final version

--- startup.js
+++ startup.js
@@ -10,9 +10,7 @@ if(loading_div){
 
 DISK.initialize_game();
 CURRENTLEVEL.setup("titlescreen");
-/*
-if (System.Web.HttpContext.Current.Request.Browser.IsMobileDevice && System.Web.HttpContext.Current.Request.UserAgent.Contains("; wv")) {
-    CONSOLE.error("It appears that your web browser is not allowed to download data files. You can continue to the game with the autosave, but you will not be able to download your save files. I recommend you use a dedicated web browser (Firefox, Opera, Chrome...) so that you can download your saves.", false, true);
-}*/
 
-alert(navigator.userAgent);
+if (navigator.userAgent.includes("; wv")) {
+    CONSOLE.error("It appears that your web browser is not allowed to download data files. You can continue to the game with the autosave, but you will not be able to download your save files. I recommend you use a dedicated web browser (Firefox, Opera, Chrome...) so that you can download your saves.", false, true);
+}
