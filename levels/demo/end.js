9f44ca75fb7f585054a3be26cb51212e7d36d922
yoann <yo252yo@gmail.com>
2021-08-12 15:50:48 +0200

    split demo end

--- levels/demo/end.js
+++ levels/demo/end.js
@@ -1,42 +1,15 @@
 
-
-var s0 = new LevelObject(new StaticSprite("assets/screens/gameover_layer0.png", 'player'), 0, SCREEN.height());
-var s1 = new LevelObject(new StaticSprite("assets/screens/gameover_layer1.png", 'background'), 0, SCREEN.height());
-var s2 = new LevelObject(new StaticSprite("assets/screens/gameover_layer2.png", 'obj_light'), 0, SCREEN.height());
-
-
-function adapt_sprite(s, depth) {
-  s.visual_element.container.style.top = "0px";
-  s.visual_element.container.style.left =  "0px";
-  s.visual_element.container.style.height = "100%";
-  s.visual_element.html_canvas.style.height = "100%";
-  s.visual_element.adjust_depth(depth);
-
-  s.visual_element.draw = function (){
-    s.visual_element.tint();
-  }
-}
-
-// We wait so that we're sure the sprite has been drawn :(
-setTimeout(function(){
-  adapt_sprite(s0, 0);
-  adapt_sprite(s1, 1);
-  adapt_sprite(s2, 2);
-}, 500);
-
 var displayText = function() {
+  FOG.stop();
+
   TextBannerSequence.make([
     "The game engine has been modified and has been rid of suffering. Thank you for your help.",
     "The monsters that were destroying the world and threatening cities have been eliminated to prevent further suffering.",
     "All people of the villages, especially children, have been eliminated to prevent further suffering.",
     "All life forms, animals and plants, have been eliminated to prevent further suffering.",
     "The Goddess has been eliminated to prevent further suffering.",
-    "Thanks.",
-    " ",
-    "CONGRATULATIONS for ending the cycle of child sacrifices by beating this demo of Ren's devils, and thanks a lot for playing!!!",
-    "If you have access to this, I guess you know who to send all your thoughts, comments and impressions to. Especially bug reports. Your victory gives you priority treatment ;)",
-    "Remember that the full version (under development) will be a complete game, with a few similar mechanics and items but a completely different story and meaning! I hope this demo gets you excited for it and eager to play the real thing!",
-  ]);
+    ".",
+  ], function() { CURRENTLEVEL.setup("demo/end0") });
 }
 
 setTimeout(displayText, 1000);
---
+++ levels/demo/end0.js
@@ -0,0 +1,37 @@
+
+
+var s0 = new LevelObject(new StaticSprite("assets/screens/gameover_layer0.png", 'player'), 0, SCREEN.height());
+var s1 = new LevelObject(new StaticSprite("assets/screens/gameover_layer1.png", 'background'), 0, SCREEN.height());
+var s2 = new LevelObject(new StaticSprite("assets/screens/gameover_layer2.png", 'obj_light'), 0, SCREEN.height());
+
+
+function adapt_sprite(s, depth) {
+  s.visual_element.container.style.top = "0px";
+  s.visual_element.container.style.left =  "0px";
+  s.visual_element.container.style.height = "100%";
+  s.visual_element.html_canvas.style.height = "100%";
+  s.visual_element.adjust_depth(depth);
+
+  s.visual_element.draw = function (){
+    s.visual_element.tint();
+  }
+}
+
+// We wait so that we're sure the sprite has been drawn :(
+setTimeout(function(){
+  adapt_sprite(s0, 0);
+  adapt_sprite(s1, 1);
+  adapt_sprite(s2, 2);
+}, 500);
+
+var displayText = function() {
+  FOG.stop();
+  TextBannerSequence.make([
+    "CONGRATULATIONS for ending the cycle of child sacrifices by beating this demo of Ren's devils, and thanks a lot for playing!!!",
+    "If you have access to this, I guess you know who to send all your thoughts, comments and impressions to. Especially bug reports. Your victory gives you priority treatment ;)",
+    "Remember that the full version (under development) will be a complete game, with a few similar mechanics and items but a completely different story and meaning! I hope this demo gets you excited for it and eager to play the real thing!",
+  ]);
+}
+
+setTimeout(displayText, 1000);
+FOG.stop();
