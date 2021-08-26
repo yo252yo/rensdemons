e7cad6546eefa83e52cddb20b0ca6df06dcd3212
yoann <yo252yo@gmail.com>
2021-08-26 15:02:03 +0200

    limits the page turn click zone on mobile

--- engine/modules/IO/IO_DIALOG.js
+++ engine/modules/IO/IO_DIALOG.js
@@ -5,13 +5,21 @@ const IO_DIALOG = {
 
   onClick: function(x,y, is_hold) {
     if (! is_hold) {
-      IO.dialog.turn_page();
+      if(!SCREEN.is_mobile()){
+        IO.dialog.turn_page();
+      } else {
+        var y_relat = (y - window.scrollY) / SCREEN.height();
+        var x_relat = (x - window.scrollX) / SCREEN.width();
+        if (y_relat > 0.5 && y_relat < 0.95 && x_relat > 0.05 && x_relat < 0.95) {
+          IO.dialog.turn_page();
+        }
+      }
     }
   },
 
   onPressKey: function(key) {
     if (KEYS_UTIL.is_ok(key)) {
       IO.dialog.turn_page();
-    }    
+    }
   },
 }
