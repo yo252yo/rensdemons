8a03e8b7ca58e973406f2e60b3bd0ec1cbf27da1
yoann <yo252yo@gmail.com>
2021-09-20 16:58:10 +0200

    shift and resizes stores

--- engine/classes/Generation/StoreGenerator.js
+++ engine/classes/Generation/StoreGenerator.js
@@ -11,22 +11,22 @@ class StoreGenerator {
     }
 
     main_entrance() {
-      var f = new S_ExitFloor(335, 525, 50, 35, this.outside);
-      return  [350, 505];
+      var f = new S_ExitFloor(1275, 1525, 50, 35, this.outside);
+      return  [1290, 1505];
     }
 
     build_seller() {
       if (Object.keys(ITEMS_ARCHETYPES).includes(this.store_type)){
-        return new M_Vendor(250, 250, this.gen.get(), this.store_type, this.store_threshold);
+        return new M_Vendor(1250, 1350, this.gen.get(), this.store_type, this.store_threshold);
       }
       if (Object.keys(ABILITIES_ARCHETYPES).includes(this.store_type)){
-        return new M_Trainer(250, 250, this.gen.get(), this.store_type, this.store_threshold);
+        return new M_Trainer(1250, 1350, this.gen.get(), this.store_type, this.store_threshold);
       }
       CONSOLE.error("Unable to allocate archetype to type " + this.store_type);
     }
 
     build() {
-      new S_WoodFloor(200, 500, 300, 300);
+      new S_WoodFloor(1200, 1500, 200, 200);
       this.build_seller();
       return this.main_entrance();
     }
