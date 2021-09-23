c90c2fdc0a22eb05da93aaac150526c19866b91b
yoann <yo252yo@gmail.com>
2021-09-23 14:45:09 +0200

    utility battle for sprite testing

---
+++ battles/_demo/_sprite.js
@@ -0,0 +1,8 @@
+
+var url_string = window.location.href
+var url = new URL(url_string);
+
+new CenteredImage("assets/battles/" + url.searchParams.get("b") + ".png", 'background');
+PLAYER_ACTIONS.allow_flight();
+
+BATTLE.operations.start("...");
