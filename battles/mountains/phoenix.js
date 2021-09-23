f0d3fb37920c7ce7555a1d8f3e088ae6980e83c0
yoann <yo252yo@gmail.com>
2021-09-23 16:37:48 +0200

    text for T2 mountains

---
+++ battles/mountains/phoenix.js
@@ -0,0 +1,55 @@
+// ===================
+//hack INITIALIZATION
+// ===================
+new CenteredImage("assets/battles/mountains/phoenix.png", 'background');
+PLAYER_ACTIONS.allow_flight();
+
+/*T2 WIP
+BATTLE.operations.add_loot(ITEM.Meat, 0.5);
+BATTLE.operations.add_loot(ITEM.Bone, 1);
+BATTLE.operations.add_loot("", 2);
+
+// ===================
+//hack PLAYER CAPABILITIES
+// ===================
+PLAYER_ACTIONS.kill_with_anything_over(300);
+
+PLAYER_ACTIONS.win(PARTYMEMBERS.StreetSmart);
+PLAYER_ACTIONS.win(PARTYMEMBERS.SavageChild);
+
+PLAYER_ACTIONS.win(ABILITY.Ice_bolt, 5);                  // 100  ELEM
+PLAYER_ACTIONS.win(ABILITY.Asphyxiate, 1);                // 500  ELEM
+
+PLAYER_ACTIONS.win(ABILITY.Petrify, 1);                   // 300  SPIR
+
+PLAYER_ACTIONS.win(ABILITY.Circumvent, 3);                // 100  DIPL
+
+PLAYER_ACTIONS.win(ITEM.Elixir_ice, 2, true);             // 50   ALCH
+PLAYER_ACTIONS.win(ITEM.Elixir_vine, 1, true);            // 75   ALCH
+
+PLAYER_ACTIONS.win(ITEM.Mace, 4);                         // 100  WEAP
+PLAYER_ACTIONS.win(ITEM.Spear, 1);                        // 250  WEAP
+
+PLAYER_ACTIONS.win(ITEM.Rope, 3);                         // 100  TOOL
+
+// ===================
+//hack MONSTER BEHAVIOR
+// ===================
+var attack = {
+  attack_amplitude: 0.3, // Between 0 and 1
+  warning_time_s: 0.2,
+  react_time_s: 1.5,
+  time_variation: 0.2, // 1 = 100%
+};*/
+
+BATTLE.monster_actions.add_textual("The Phoenix opens its beak wide and lets out a single note. It seems that the air around you withers and contracts under the mysterious melancholy of its ethereal timbre, making it hard to breathe.", attack);
+BATTLE.monster_actions.add_textual("The Phoenix lets out a mighty shriek which clearly echoes miles and miles away. The sharp sound pierces through the ears and the brains of everyone in the vicinity.", attack);
+BATTLE.monster_actions.add_textual("The Phoenix whips you with its incandescent tail. A tornado of embers surround you, heating up the air and scorching your skin.", attack);
+
+
+
+// ===================
+//hack START
+// ===================
+
+BATTLE.operations.start("A Pompous Phoenix Parades or Patrols.");
---
+++ battles/mountains/pterosaur.js
@@ -0,0 +1,55 @@
+// ===================
+//hack INITIALIZATION
+// ===================
+new CenteredImage("assets/battles/mountains/pterosaur.png", 'background');
+PLAYER_ACTIONS.allow_flight();
+
+/*T2 WIP
+BATTLE.operations.add_loot(ITEM.Meat, 0.5);
+BATTLE.operations.add_loot(ITEM.Bone, 1);
+BATTLE.operations.add_loot("", 2);
+
+// ===================
+//hack PLAYER CAPABILITIES
+// ===================
+PLAYER_ACTIONS.kill_with_anything_over(300);
+
+PLAYER_ACTIONS.win(PARTYMEMBERS.StreetSmart);
+PLAYER_ACTIONS.win(PARTYMEMBERS.SavageChild);
+
+PLAYER_ACTIONS.win(ABILITY.Ice_bolt, 5);                  // 100  ELEM
+PLAYER_ACTIONS.win(ABILITY.Asphyxiate, 1);                // 500  ELEM
+
+PLAYER_ACTIONS.win(ABILITY.Petrify, 1);                   // 300  SPIR
+
+PLAYER_ACTIONS.win(ABILITY.Circumvent, 3);                // 100  DIPL
+
+PLAYER_ACTIONS.win(ITEM.Elixir_ice, 2, true);             // 50   ALCH
+PLAYER_ACTIONS.win(ITEM.Elixir_vine, 1, true);            // 75   ALCH
+
+PLAYER_ACTIONS.win(ITEM.Mace, 4);                         // 100  WEAP
+PLAYER_ACTIONS.win(ITEM.Spear, 1);                        // 250  WEAP
+
+PLAYER_ACTIONS.win(ITEM.Rope, 3);                         // 100  TOOL
+
+// ===================
+//hack MONSTER BEHAVIOR
+// ===================
+var attack = {
+  attack_amplitude: 0.3, // Between 0 and 1
+  warning_time_s: 0.2,
+  react_time_s: 1.5,
+  time_variation: 0.2, // 1 = 100%
+};*/
+
+BATTLE.monster_actions.add_textual("The Pterosaur bits you, burrowing its fangs in your legs.", attack);
+BATTLE.monster_actions.add_textual("The Pterosaur snaps its powerful jaw. Unlike regular birds, it's filled with sharp teeth that cry out for your flesh.", attack);
+BATTLE.monster_actions.add_textual("The Pterosaur bites the air in your direction.", attack);
+
+
+
+// ===================
+//hack START
+// ===================
+
+BATTLE.operations.start("A Predatory Pterosaur Pierces through the Panorama.");
