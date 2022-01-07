43351bb322ec2ce332d139dca3a94088a5bc49c8
yoann <yo252yo@gmail.com>
2022-01-07 19:16:51 +0100

    give all NPCs a backstory

--- engine/modules/BAGGAGE/LEDGER.js
+++ engine/modules/BAGGAGE/LEDGER.js
@@ -118,18 +118,58 @@ const LEDGER = {
 
   getBio: function(){
     var villager;
+    var throwaway = true;
     if (Math.random() < 0.2){
       villager = LEDGER.get_throwaway_villager(Math.random());
     } else{
       villager = LEDGER.get_villager(Math.random());
+      throwaway = false;
     }
     var birth = (new Date(villager.birth)).toLocaleString();
     var death = (new Date(villager.death)).toLocaleString();
+    var gen = new Generator(villager.name);
+
+    var male = DATASETS.male_names.includes(villager.name);
+    var man = male ? "man" : "woman";
+    var pronoun = male ? "he" : "she";
+    var possessive = male ? "his" : "her";
+
+    var adj = gen.pick(["brave", "loving", "strong", "smart", "wealthy", "caring", "popular", "kind", "zealous", "lazy", "talentless", "friendly", "despised", "funny"]);
+    var parents = gen.pick(["loving", "uncaring", "indifferent", "severe", "wealthy", "happy", "demanding", "sick", "devout", "conscientious", "strict"]);
+    var childhood = gen.pick(["happy", "difficult", "tough", "quiet", "peaceful", "challenging", "harsh", "uneventful", "sad", "poor", "relaxed"]);
+    var pursuit = gen.pick(["perfection", "strength", "knowledge", "virtue", "truth", "peace", "enlightnement", "devotion", "piety", "power", "influence"]);
+    var witness = gen.pick(["colleagues", "friends", "family", "countrymen", "acquaintances", "cousins", "neighbours", "lovers", "clients"]);
+    var passion = gen.pick(["fruits", "birds", "fishing", "sports", "helping others", "religion", "flowers", "weapons", "art", "music", "cooking", "wine", "intimate relationships", "meditation", "poetry", "hunting", "fighting", "sculpting", "puppets", "gambling", "archery"]);
+    var adverb = gen.pick(["peacefully", "fearcely", "elegantly", "miserably", "cautiously", "boldly", "blissfully", "arrogantly", "enthusiastically", "randomly"]);
+    var events = gen.pick([
+      `${pronoun} witnessed ${possessive} parents die to a monster attack`,
+      `${pronoun} got seduced by a noble`,
+      `${pronoun} was hired as an apprentice by a famous craftsman`,
+      `${pronoun} got betrayed by ${possessive} best friend`,
+      `${possessive} father died in the war`,
+      `${pronoun} got trampled by a horse`,
+      `${pronoun} made a fortune gambling`,
+      `${pronoun} fell in love`,
+      `${pronoun} got beaten by ${possessive} uncle`,
+      `${pronoun} spoke with the Goddess in a dream`,
+      `${possessive} family lost all their money and possessions`,
+      `the monster armies raided ${possessive} village`
+    ]);
+    var meeting = "";
+    if(!throwaway){
+      meeting = `The highlight of ${possessive} life is that blessed day in which ${pronoun} had the chance to see the Promised Child. It was the happiest day of ${possessive} life, but also the last.<br />`;
+    }
+
     return `<h1>${villager.name}</h1>
     <hr>
     <h5>Not from Wikipedia, the free encyclopedia</h5>
     <strong>Birth</strong>: ${birth}, in ${villager.city}, ${DICTIONARY.get("world_name")}<br />
     <strong>Death</strong>: ${death}, in ${villager.city}, ${DICTIONARY.get("world_name")}
-    `;
+    <hr>
+    ${villager.name} was a ${adj} ${man}. Born by the will of our God in ${villager.city} to ${parents} parents, ${pronoun} had a ${childhood} childhood.<br />
+    Life completely changed for ${villager.name} when ${events}. From that day on, ${pronoun} decided to dedicate ${possessive} existence to the pursuit of ${pursuit}.<br />
+    Of course, ${villager.name}'s brief life was not summarized by one simple trait. ${villager.name} lead a rich and fullfilling existence, most notably, ${possessive} ${witness} remember ${villager.name} fondly for ${possessive} passion for ${passion}.<br />
+    ${meeting}
+    ${villager.name} died as ${pronoun} lived, ${adverb}. Our God decided that ${pronoun} was no longer needed, and removed ${villager.name} from existence, but not from our memories.`;
   }
 }
