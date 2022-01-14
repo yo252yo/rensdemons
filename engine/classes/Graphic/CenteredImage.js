c4ffb36307b996739187d2c17cca3408835aad70
yoann <yo252yo@gmail.com>
2022-01-14 17:39:52 +0100

    fix a dodging sprite problem

--- engine/classes/Graphic/CenteredImage.js
+++ engine/classes/Graphic/CenteredImage.js
@@ -9,6 +9,8 @@ class CenteredImage extends FixedSprite {
     draw() {
       super.draw();
       this.adjust_dimensions(this.width * this.scale, this.height * this.scale);
+      this.width = this.width || this.resource.width;
+      this.height = this.height || this.resource.height;
       if(! this.lvl_obj){
         this.lvl_obj = new LevelObject(this, SCREEN.width() / 2 - this.resource.width / 2, Math.max(SCREEN.height() / 2 - 50, this.resource.height));
       } else{
