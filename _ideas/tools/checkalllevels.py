d164b6e79f4f603db63f1d444a9ef54bafe6aace
yoann <yo252yo@gmail.com>
2022-02-22 18:49:03 +0100

    delete the WIP ideas folder to avoid including it by mistake

--- _ideas/tools/checkalllevels.py
+++
@@ -1,9 +0,0 @@
-import os
-import re
-
-oroot = os.path.dirname(os.getcwd())
-dir = os.path.join(oroot, "levels")
-
-for file in os.listdir(dir):
-        if ".js" in file:
-            print("<a href='file:///D:/work/rensdemons/debug.html?lvl=" + file[:-3] + "'> " + file[:-3] + "</a><br />")
