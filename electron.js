ed154f6ca4fe860b8ba7b246435114057009166c
yoann <yo252yo@gmail.com>
2022-02-23 13:07:22 +0100

    hide menu bar in electron

--- electron.js
+++ electron.js
@@ -2,9 +2,10 @@ const { app, BrowserWindow } = require('electron')
 
 const createWindow = () => {
   const win = new BrowserWindow({
-    width: 800,
-    height: 600
+    width: 1024,
+    height: 768,
   })
+  win.setMenuBarVisibility(false);
 
   win.loadFile('index.html')
 }
