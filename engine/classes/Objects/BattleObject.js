5408e99508e711c4bfc410ed0e14601fbcdb6870
yoann <yo252yo@gmail.com>
2022-03-07 17:32:24 +0100

    change default max actions of battleobject to 1

--- engine/classes/Objects/BattleObject.js
+++ engine/classes/Objects/BattleObject.js
@@ -8,7 +8,7 @@ class BattleObject extends LevelObject {
       var g = new Generator(x+y);
       this.interactions = {};
       this.special_effect = {};
-      this.max_actions = max_actions || 2;
+      this.max_actions = max_actions || 1;
       this.seeds = [];
       this.enemy_actions = [];
       for(var i = 0; i< this.max_actions; i++){
