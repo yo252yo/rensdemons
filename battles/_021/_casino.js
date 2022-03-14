887928565ed19037c9e1bda24f3c5160bf21ac3c
yoann <yo252yo@gmail.com>
2022-03-14 23:27:42 +0100

    fix casino's inquire

--- battles/_021/_casino.js
+++ battles/_021/_casino.js
@@ -11,22 +11,6 @@ var record_generator = function(){
   gen = new Generator(g);
 }
 
-PLAYER_ACTIONS.add({
-  name: "Inquire",
-  unlock: true,
-  outcome: BATTLETREE.NOTHING,
-  description: [
-    `You make your way to someone you assume to be part of the organization team running this joint and you ask.`,
-    `$$Ren$: "Excuse me, what kind of randomness do you use here?"`,
-    `Employee: "Well, as you can see, cards and dice. There's occasional fights and races of animals or humans, too..."`,
-    `$$Ren$: "Sure, but is it true randomness or pseudo randomness?"`,
-    `Employee: "What do you mean?"`,
-    `$$Ren$: "If I were to come back another time, would I get the same results as this time? Does the same action twice get the same outcome? Are the dice roll deterministic? How good is your randomness generator?"`,
-    `Employee: "I'm sorry, I'm not sure I understand. Are you accusing us of cheating?"`,
-    `$$Ren$: "Sorry, nevermind."`,
-    ],
-});
-
 var unlock_escape = PLAYER_ACTIONS.function.unlock_replacing_action({
   name: ABILITY.Escape,
   description: RANDOM.pick([`You make your way out of the building, with the ${bet} coins of your bet.`]),
@@ -211,6 +195,25 @@ var main_menu = function(){
 }
 
 main_menu();
+
+
+PLAYER_ACTIONS.add({
+  name: "Inquire",
+  unlock: true,
+  outcome: BATTLETREE.NOTHING,
+  description: [
+    `You make your way to someone you assume to be part of the organization team running this joint and you ask.`,
+    `$$Ren$: "Excuse me, what kind of randomness do you use here?"`,
+    `Employee: "Well, as you can see, cards and dice. There's occasional fights and races of animals or humans, too..."`,
+    `$$Ren$: "Sure, but is it true randomness or pseudo randomness?"`,
+    `Employee: "What do you mean?"`,
+    `$$Ren$: "If I were to come back another time, would I get the same results as this time? Does the same action twice get the same outcome? Are the dice roll deterministic? How good is your randomness generator?"`,
+    `Employee: "I'm sorry, I'm not sure I understand. Are you accusing us of cheating?"`,
+    `$$Ren$: "Sorry, nevermind."`,
+    ],
+});
+
+
 BATTLE.operations.start([
   "The building houses what appears to be a casino. The atmosphere is chaotic. Patrons are betting loudly against each others in heated battles of cards or dice. A few ruined gamblers are sobbing in a corner.",
   `$$Ren$: "Of course, there is a casino..."`,
