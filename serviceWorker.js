6bbed820edc989e2e8c1ede649926e59e37e89b5
yoann <yo252yo@gmail.com>
2022-04-02 22:46:03 +0200

    cache everything in serviceworker

--- serviceWorker.js
+++ serviceWorker.js
@@ -3,11 +3,15 @@ self.addEventListener('install', (event) => {
   event.waitUntil(
     (async () => {
       const cache = await caches.open('rd');
-      const all = await cache.addAll(['index.html']);
-      const index = await cache.add(new Request("index.html", {cache: 'reload'}));
+      const all = await cache.addAll([
+        'index.html',
+        'import_manager.js',
+      ]);
+    //  const index = await cache.add(new Request("index.html", {cache: 'reload'}));
     })()
   );
 });
+
 
 self.addEventListener('activate', (event) => {});
 
@@ -16,6 +20,7 @@ self.addEventListener('fetch', function(event) {
    (async () => {
      try {
        const networkResponse = await fetch(event.request);
+       await cache.add(networkResponse);
        return networkResponse;
      } catch (error) {
        const cache = await caches.open('rd');
