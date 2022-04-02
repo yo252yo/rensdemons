19242b624107b0b3878deedefd234c98aff73ea8
yoann <yo252yo@gmail.com>
2022-04-02 20:36:26 +0200

    more service worker fix

--- serviceWorker.js
+++ serviceWorker.js
@@ -5,15 +5,23 @@ self.addEventListener('install', (event) => {
     await cache.addAll([
       'index.html',
     ]);
+    await cache.add(new Request("index.html", {cache: 'reload'}));
   })());
 });
 
 self.addEventListener('activate', (event) => {});
 
 self.addEventListener('fetch', function(event) {
- event.respondWith((async () => {
-   caches.match(event.request).then(function(response) {
-     return response || fetch(event.request);
+ event.respondWith(
+   (async () => {
+     try {
+       const networkResponse = await fetch(event.request);
+       return networkResponse;
+     } catch (error) {
+       const cache = await caches.open('rd');
+       const cachedResponse = await cache.match(event.request);
+       return cachedResponse;
+     }
    });
  })());
 });
