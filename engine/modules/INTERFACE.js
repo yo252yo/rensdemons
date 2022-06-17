4fc12ca8df3531c428acdfd91b08d8e88f4aff3a
yoann <yo252yo@gmail.com>
2022-06-17 22:41:17 +0200

    puts the demo version as default online

--- engine/modules/INTERFACE.js
+++ engine/modules/INTERFACE.js
@@ -344,7 +344,7 @@ const INTERFACE = {
   },
 
   is_trial: function(){
-    return window.location.href.includes("trial");
+    return window.location.href.includes("yo252yo.com") && !window.location.href.includes("winarg");
   },
 
   start_game: function(){
